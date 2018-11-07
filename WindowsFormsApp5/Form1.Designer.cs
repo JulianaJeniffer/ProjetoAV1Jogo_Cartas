576f1ebf26eb4f11caf97b78ab71cad42af2818b
Juliana Jeniffer <juliana.jmviana@gmail.com>
2018-11-06 23:57:17 -0200

    Primeiro commit

---
+++ WindowsFormsApp5/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace WindowsFormsApp5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btnGerarNaipe = new System.Windows.Forms.Button();
+            this.btnGerarCarta = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(109, 74);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(125, 151);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(340, 74);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(125, 151);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // btnGerarNaipe
+            // 
+            this.btnGerarNaipe.Location = new System.Drawing.Point(126, 243);
+            this.btnGerarNaipe.Name = "btnGerarNaipe";
+            this.btnGerarNaipe.Size = new System.Drawing.Size(75, 23);
+            this.btnGerarNaipe.TabIndex = 2;
+            this.btnGerarNaipe.Text = "button1";
+            this.btnGerarNaipe.UseVisualStyleBackColor = true;
+            // 
+            // btnGerarCarta
+            // 
+            this.btnGerarCarta.Location = new System.Drawing.Point(369, 243);
+            this.btnGerarCarta.Name = "btnGerarCarta";
+            this.btnGerarCarta.Size = new System.Drawing.Size(75, 23);
+            this.btnGerarCarta.TabIndex = 3;
+            this.btnGerarCarta.Text = "button2";
+            this.btnGerarCarta.UseVisualStyleBackColor = true;
+            this.btnGerarCarta.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(574, 391);
+            this.Controls.Add(this.btnGerarCarta);
+            this.Controls.Add(this.btnGerarNaipe);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btnGerarNaipe;
+        private System.Windows.Forms.Button btnGerarCarta;
+    }
+}
+
---
+++ WindowsFormsApp5/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int carta2 = bntGerarCarta();
+            int naipe2 = bntGerarNaipe();
+            //MessageBox.Show("carta "+carta2+" naipe "+naipe2); // debug
+            string nomeNaipe = "";
+            // Troca o numero do naipe pela letra do nome 
+            if (naipe2 == 1) nomeNaipe = "o";
+            else if (naipe2 == 2) nomeNaipe = "p";
+            else if (naipe2 == 3) nomeNaipe = "c";
+            else nomeNaipe = "e";
+            // monta o nome
+            string nomeCarta2 = ("" + carta2 + "_" + nomeNaipe + ".png");
+            pictureBox2.Load(@"..\..\imagens\" + nomeCarta2);
+
+        }
+
+        private int bntGerarCarta()
+        {
+            Random randCarta = new Random();
+            return (randCarta.Next(1, 11));
+        }
+        private int bntGerarNaipe()
+        {
+            Random randNaipe= new Random();
+            return (randNaipe.Next(1, 5));
+        }
+            
+    }
+}
